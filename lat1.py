2e4b2b01bfa54a7e985bcdb65f205dca6b839dc6
Syisyi <kalaeravg@gmail.com>
2024-02-16 22:58:37 -0800

    v1

---
+++ lat1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Syisyi's")
+df = pd.DataFrame({
+  'No': [1, 2, 3, 4],
+  'Name': ['Rafayel', 'Marius', 'DanHeng', 'Freminet'],
+  'Age': ['24', '21', '27', '20']
+})
+
+df
