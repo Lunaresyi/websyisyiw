2cc590e10c80a7fae99228868fe6f0d4b9dd0859
Syisyi <kalaeravg@gmail.com>
2024-02-28 20:19:30 -0800

    lagi

--- lat1.py
+++ lat1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
-st.write("Syisyi's")
+st.write("Syisyi's Interest")
 df = pd.DataFrame({
   'No': [1, 2, 3, 4],
   'Name': ['Rafayel', 'Marius', 'DanHeng', 'Freminet'],
