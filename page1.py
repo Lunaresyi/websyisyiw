cee031886d8424fcfdafc9e354d81e6de4057859
Syisyi <kalaeravg@gmail.com>
2024-03-01 20:50:19 -0800

    dhfvg

--- page1.py
+++ page1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def page_1():
-  st.markdown("<h1 style='text-align: center; color: grey;'>Page One</h1>", unsafe_allow_html=True)
+  st.markdown("<h1 style='text-align: center; color: Black;'>Page One</h1>", unsafe_allow_html=True)
   st.markdown("<h2 style='text-align: center; color: grey;'>!!✩ iNTRODUCTION ✩!!</h2>", unsafe_allow_html=True)
-  st.markdown('<div style="text-align: center;">This is me if you care!!! :3</div>', unsafe_allow_html=True)
+  st.markdown('<div style="text-align: center;">Syisyi ; She/Her, 17, NE DOM, Melancholy Sanguine, 279 as tritypes, sx/so, 2w3! I love cats, I put my eyes on Gacha Games and RPGs, I watch anime, movies, I read many manga and Webtoon! 🌟_🌟</div>', unsafe_allow_html=True)
