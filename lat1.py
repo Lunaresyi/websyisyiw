d6dc6fcbe50790e0441bf9e2e6795480ea5326e6
Syisyi <kalaeravg@gmail.com>
2024-03-01 18:43:45 -0800

    hahah

--- lat1.py
+++ lat1.py
@@ -27,6 +27,6 @@ PAGES = {
   "Page Three" : page_3
 }
 
-st.sidebar.image("syisyiii.png", width=100)
+st.sidebar.image("hutaw.jpg", width=200)
 page = st.sidebar.radio("Pages List :", list(PAGES.keys()))
 PAGES[page]()
