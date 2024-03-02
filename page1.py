8f4b7a598ccdf44eb9ca0cbbb1dfcdbf414b823f
Syisyi <kalaeravg@gmail.com>
2024-03-01 20:10:31 -0800

    ddguedg

--- page1.py
+++ page1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 def page_1():
-  st.title("Page One")
-  st.write("This page is for Introduction")
-  st.image("hutaw.jpg", width=400)
-  st.write("This is me if u care :3!!!")
+  st.markdown("<h1 style='text-align: center; color: grey;'>Page One</h1>", unsafe_allow_html=True)
+  st.markdown("<h2 style='text-align: center; color: grey;'>!!✩ iNTRODUCTION ✩!!</h2>", unsafe_allow_html=True)
+  st.markdown('<div style="text-align: center;">This is me if you care!!! :3</div>', unsafe_allow_html=True)
